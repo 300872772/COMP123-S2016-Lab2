c916c58a43e65b717420dc3936bf7b13aad61411
300872772 <mamun_131@hotmail.com>
2016-05-24 14:20:03 -0400

    Updated Person class to include Name peroperty

--- COMP123-S2016-Lession3/Program.cs
+++ COMP123-S2016-Lession3/Program.cs
@@ -10,15 +10,37 @@ namespace COMP123_S2016_Lession3
 {
 
     /**
+     * this class is the driver class for our Program
      * 
+     * @class Program
      * 
      */
    public class Program
 
 
     {
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {string []} args
+         * 
+         */
+
        public static void Main(string[] args)
         {
+            //crat a new instance of Person class
+            Person person;
+            person = new Person("Tom");
+
+            Console.WriteLine(person.Name);
+
+            //set a value in the Name property of the Person class
+          //  person.Name = "Tom";
+
+            //get the Name value and print to console
+
+            Console.WriteLine(person.Name);
 
         }
 
